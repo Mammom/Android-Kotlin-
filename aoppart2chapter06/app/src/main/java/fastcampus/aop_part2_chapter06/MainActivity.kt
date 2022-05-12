7ba7b2e7b47a0e1a353ad43f861f806eb7d427d0
DESKTOP-AUPBQJO\kimco <kimcomcom0@daum.net>
2022-05-12 23:09:17 +0900

    [Andorid] Kotlin

---
+++ aoppart2chapter06/app/src/main/java/fastcampus/aop_part2_chapter06/MainActivity.kt
@@ -0,0 +1,47 @@
+package fastcampus.aop_part2_chapter06
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private val remainMinutesTextView :TextView by lazy {
+        findViewById(R.id.remainMinutesTextView)
+    }
+
+    private val remainSecondsTextView :TextView by lazy {
+        findViewById(R.id.remainSecondsTextView)
+    }
+
+    private val seekBar :SeekBar by lazy {
+        findViewById(R.id.seekBar)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bindViews()
+    }
+
+    private fun bindViews(){
+        seekBar.setOnSeekBarChangeListener(
+            object : SeekBar.OnSeekBarChangeListener{
+                override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                    remainMinutesTextView.text = "%02d".format(p1)
+                }
+
+                override fun onStartTrackingTouch(p0: SeekBar?) {
+
+                }
+
+                override fun onStopTrackingTouch(p0: SeekBar?) {
+
+                }
+            }
+        )
+    }
+
+}
