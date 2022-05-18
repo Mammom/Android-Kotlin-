242f149eb8693e88eee75387e8290ca77685aa3a
DESKTOP-AUPBQJO\kimco <kimcomcom0@daum.net>
2022-05-18 22:32:01 +0900

    [Android] Kotlin

--- aoppart2chapter07/app/src/main/java/fastcampus/aop/part2/chapter07/MainActivity.kt
+++ aoppart2chapter07/app/src/main/java/fastcampus/aop/part2/chapter07/MainActivity.kt
@@ -1,25 +1,60 @@
 package fastcampus.aop.part2.chapter07
 
+import android.content.pm.PackageManager
+import android.media.MediaRecorder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
+
+
 
     private val recordButton:RecordButton by lazy {
         findViewById(R.id.recorButton)
     }
 
-    private var stare = State.BEFORE_RECORDING
+    private val requiredPermissions = arrayOf(android.Manifest.permission.RECORD_AUDIO)
+
+    private var recorder:MediaRecorder? = null
+
+    private var state = State.BEFORE_RECORDING
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        requestAudioPermission()
+
         initView()
     }
 
-    fun initView(){
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        val audioRecordPermissionGranted =
+            requestCode == REQUEST_RECORD_AUDIO_PERMISSION &&
+                grantResults.firstOrNull() == PackageManager.PERMISSION_GRANTED
+
+        if(!audioRecordPermissionGranted){
+            finish()
+        }
+
+    }
+
+    private fun requestAudioPermission(){
+        requestPermissions(requiredPermissions, REQUEST_RECORD_AUDIO_PERMISSION)
+    }
+
+    private fun initView(){
         recordButton.updateIconWithState(state)
     }
 
+    companion object{
+        private const val REQUEST_RECORD_AUDIO_PERMISSION = 201
+    }
 }
