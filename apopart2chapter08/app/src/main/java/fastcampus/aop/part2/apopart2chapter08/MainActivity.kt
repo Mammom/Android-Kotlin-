bc0c939e2735a8a15484956dc29ef771b14930b5
DESKTOP-AUPBQJO\kimco <kimcomcom0@daum.net>
2022-06-23 22:13:58 +0900

    [Android]kotlin

--- apopart2chapter08/app/src/main/java/fastcampus/aop/part2/apopart2chapter08/MainActivity.kt
+++ apopart2chapter08/app/src/main/java/fastcampus/aop/part2/apopart2chapter08/MainActivity.kt
@@ -3,10 +3,16 @@ package fastcampus.aop.part2.apopart2chapter08
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    private val addressBar :EditText by lazy {
+        findViewById(R.id.addressBar)
+    }
 
     private val webView: WebView by lazy {
         findViewById(R.id.webView)
@@ -16,12 +22,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initViews()
+        bindView()
     }
 
     @SuppressLint("SetJavaScriptEnabled")
     private fun initViews(){
-        webView.webViewClient = WebViewClient()
-        webView.settings.javaScriptEnabled = true
-        webView.loadUrl("https://www.google.com")
+        webView.apply {
+            webViewClient = WebViewClient()
+            settings.javaScriptEnabled = true
+            loadUrl("https://www.google.com")
+        }
+    }
+
+    private fun bindView(){
+        addressBar.setOnEditorActionListener { v, actionId, event ->
+            if(actionId == EditorInfo.IME_ACTION_DONE){
+                webView.loadUrl(v.text.toString())
+            }
+
+            return@setOnEditorActionListener false
+        }
     }
 }
