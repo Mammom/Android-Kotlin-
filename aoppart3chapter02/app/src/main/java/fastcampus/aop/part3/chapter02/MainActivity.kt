b20a48c19472c865cfe426ab79465cf19b2c84cf
DESKTOP-AUPBQJO\kimco <kimcomcom0@daum.net>
2022-07-04 19:37:26 +0900

    [Android]kotlin

--- aoppart3chapter02/app/src/main/java/fastcampus/aop/part3/chapter02/MainActivity.kt
+++ aoppart3chapter02/app/src/main/java/fastcampus/aop/part3/chapter02/MainActivity.kt
@@ -19,9 +19,11 @@ class MainActivity : AppCompatActivity() {
     private fun initViews(){
         viewPager.adapter = QuotesPagerAdapter(
             listOf(
+                
                     Quote(
                 "-나는 생각한다. 고로 나는 존재한다.",
                 "-데카르트"
+
             )
             )
         )
