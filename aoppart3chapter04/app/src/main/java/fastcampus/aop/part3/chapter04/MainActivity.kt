3d8878097fafecc85c552ef497c1993af54b9856
DESKTOP-AUPBQJO\kimco <kimcomcom0@daum.net>
2022-07-19 19:48:00 +0900

    [Android]kotlin

--- aoppart3chapter04/app/src/main/java/fastcampus/aop/part3/chapter04/MainActivity.kt
+++ aoppart3chapter04/app/src/main/java/fastcampus/aop/part3/chapter04/MainActivity.kt
@@ -1,11 +1,109 @@
 package fastcampus.aop.part3.chapter04
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.KeyEvent
+import android.view.MotionEvent
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import fastcampus.aop.part3.chapter04.adapter.BookAdapter
+import fastcampus.aop.part3.chapter04.api.BookService
+import fastcampus.aop.part3.chapter04.databinding.ActivityMainBinding
+import fastcampus.aop.part3.chapter04.model.BestSellerDto
+import fastcampus.aop.part3.chapter04.model.SearchBookDto
+import retrofit2.*
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: BookAdapter
+    private lateinit var bookService: BookService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initBookRecyclerView()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://book.interpark.com")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        bookService = retrofit.create(BookService::class.java)
+
+        bookService.getBestSellerBooks(getString(R.string.interparkAPIKey)) //api키
+            .enqueue(object : Callback<BestSellerDto> {
+
+                override fun onResponse(
+                    call: Call<BestSellerDto>,
+                    response: Response<BestSellerDto>
+                ) {
+                    // 성공
+
+                    if (response.isSuccessful.not()) {
+                        Log.e(TAG, "NOT!! success")
+                        return
+                    }
+                    response.body()?.let {
+                        adapter.submitList(it.books)
+                    }
+                }
+
+                override fun onFailure(call: Call<BestSellerDto>, t: Throwable) {
+                    Log.e(TAG, t.toString())
+                }
+
+            })
+
+        binding.searchEditText.setOnKeyListener { _, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == MotionEvent.ACTION_DOWN) {
+                search(binding.searchEditText.text.toString())
+                return@setOnKeyListener true
+            }
+            return@setOnKeyListener false
+        }
     }
+
+    private fun search(keyword: String) {
+        bookService.getBooksByName(getString(R.string.interparkAPIKey), keyword)
+            .enqueue(object : Callback<SearchBookDto> {
+
+                override fun onResponse(
+                    call: Call<SearchBookDto>,
+                    response: Response<SearchBookDto>
+                ) {
+                    if (response.isSuccessful.not()) {
+                        Log.e(TAG, "NOT!! success")
+                        return
+                    }
+                    response.body()?.let {
+                        adapter.submitList(it.books)
+                    }
+                }
+
+                override fun onFailure(call: Call<SearchBookDto>, t: Throwable) {
+                    Log.e(TAG, t.toString())
+                }
+
+            })
+
+    }
+
+
+    private fun initBookRecyclerView() {
+        adapter = BookAdapter()
+
+        binding.bookRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.bookRecyclerView.adapter = adapter
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+
 }
--- aoppart3chapter04/app/src/main/java/fastcampus/aop/part3/chapter04/api/BookService.kt
+++ aoppart3chapter04/app/src/main/java/fastcampus/aop/part3/chapter04/api/BookService.kt
@@ -1,16 +1,17 @@
 package fastcampus.aop.part3.chapter04.api
 
 import fastcampus.aop.part3.chapter04.model.BestSellerDto
+import fastcampus.aop.part3.chapter04.model.SearchBookDto
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface BookService {
-    @GET("/api/search.api?output=jeson")
+    @GET("/api/search.api?output=json")
     fun getBooksByName(
         @Query("key") apiKey:String,
         @Query("query") keyword:String
-    ): Call<>
+    ): Call<SearchBookDto>
 
     @GET("/api/bestSeller.api?output=json&categoryId=100")
     fun getBestSellerBooks(
