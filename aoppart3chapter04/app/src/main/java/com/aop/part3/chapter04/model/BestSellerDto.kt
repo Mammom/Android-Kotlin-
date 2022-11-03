da36b9be0e5c451b4bc84d65d849bbd01ced1142
DESKTOP-AUPBQJO\kimco <kimcomcom0@daum.net>
2022-11-03 18:58:49 +0900

    [Android] Restart

--- aoppart3chapter04/app/src/main/java/com/aop/part3/chapter04/model/BestSellerDto.kt
+++
@@ -1,8 +0,0 @@
-package com.aop.part3.chapter04.model
-
-import com.google.gson.annotations.SerializedName
-
-data class BestSellerDto (
-    @SerializedName("title") val title: String,
-    @SerializedName("item") val books: List<Book>,
-)
--- aoppart3chapter04/app/src/main/java/com/aop/part3/chapter04/model/SearchBookDto.kt
+++
@@ -1,8 +0,0 @@
-package com.aop.part3.chapter04.model
-
-import com.google.gson.annotations.SerializedName
-
-data class SearchBookDto(
-    @SerializedName("title") val title:String,
-    @SerializedName("item") val books:List<Book>
-)
