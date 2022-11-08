b7fb11b97fa2b6b70404b7f06ff0bbc85843465e
DESKTOP-AUPBQJO\kimco <kimcomcom0@daum.net>
2022-11-08 19:41:31 +0900

    [Android]kotlin

---
+++ aop-part3-chapter04/app/src/main/java/fastcampus/aop/part3/chapter04/api/BookService.kt
@@ -0,0 +1,20 @@
+package fastcampus.aop.part3.chapter04.api
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface BookService {
+
+    @GET("/api/search.api?output=json")
+    fun getBooksByName(
+        @Query("key") apiKey: String,
+        @Query("query") keyword: String
+    ): Call<>
+
+    @GET("/api/bestSeller.api?output=json&categoryId=100")
+    fun getBestSellerBooks(
+        @Query("key") apiKey: String
+    ): Call<>
+
+}
---
+++ aop-part3-chapter04/app/src/main/java/fastcampus/aop/part3/chapter04/model/Book.kt
@@ -0,0 +1,5 @@
+package fastcampus.aop.part3.chapter04.model
+
+data class Book (
+    val
+)
